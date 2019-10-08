5f723c8a45c912086a244b48653d3dc472a4fd47
ArtemiiLulevich <artemii.lulevich@gmail.com>
2019-10-08 21:18:54 +0300

    Add bullets, and logic.

--- game_function.py
+++ game_function.py
@@ -1,14 +1,18 @@
 import sys
 
 import pygame
+from bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Check KEYDOWN event"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Creating a new bullet and adding it top bullets group.
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -19,20 +23,39 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Check mouse and keypad events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Refreshes the image on the screen and displays a new screen."""
     screen.fill(ai_settings.bg_color)
+
+    for bullet in bullets:
+        bullet.draw_bullet()
+
     ship.blitship()
     # Displays the last screen drawn.
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    bullets.update()
+
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    # print(len(bullets))
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
--- settings.py
+++ settings.py
@@ -4,6 +4,13 @@ class Settings:
     def __init__(self):
         self.screen_width = 800
         self.screen_height = 600
-        self.bg_color = (0, 0, 230)
+        self.bg_color = (230, 230, 250)
 
         self.ship_speed_factor = 0.75
+
+        # bullet parameters
+        self.bullet_speed_factor = 0.5
+        self.bullet_width = 5
+        self.bullet_height = 25
+        self.bullet_color = 255, 0, 0
+        self.bullets_allowed = 3
